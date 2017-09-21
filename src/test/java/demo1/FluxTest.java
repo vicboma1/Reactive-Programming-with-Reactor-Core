6ca3e9fd891e298ab851a9c1d03eec2d36957af7
vicboma1 <victorbolinchesmarin@gmail.com>
2017-09-21 15:41:39 +0200

    non/blocking stream, toArray, toStream test

--- src/test/java/demo1/FluxTest.java
+++ src/test/java/demo1/FluxTest.java
@@ -8,6 +8,7 @@ import org.mockito.internal.util.collections.Sets;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+import java.time.Duration;
 import java.util.*;
 
 public class FluxTest {
@@ -109,7 +110,7 @@ public class FluxTest {
     }
 
     @Test
-    public void restoringMissingLetter() {
+    public void restoringMissingLetterIterable() {
         final List<String> result = List.of(
                 " 1.a",
                 " 2.b",
@@ -151,4 +152,121 @@ public class FluxTest {
         Assert.assertTrue(expected.isEmpty());
     }
 
+    @Test
+    public void restoringMissingLetterStream() {
+        final List<String> result = List.of(
+                " 1.a",
+                " 2.b",
+                " 3.d",
+                " 4.e",
+                " 5.f",
+                " 6.g",
+                " 7.h",
+                " 8.j",
+                " 9.l",
+                "10.m",
+                "11.n",
+                "12.o",
+                "13.p",
+                "14.r",
+                "15.s",
+                "16.t",
+                "17.u",
+                "18.v",
+                "19.w",
+                "20.x",
+                "21.y",
+                "22.z");
+
+        final List<String> expected = new ArrayList();
+        final Mono<String> missing = Mono.just("s");
+
+        Flux.fromStream(words.stream())
+                .flatMap(word -> Flux.fromArray(word.split("")))
+                .concatWith(missing)
+                .distinct()
+                .sort()
+                .zipWith(Flux.range(1, Integer.MAX_VALUE), (string, count) -> String.format("%2d.%s", count, string))
+                .subscribe(expected::add);
+
+        result.stream()
+                .forEach(expected::remove);
+
+        Assert.assertTrue(expected.isEmpty());
+    }
+
+    @Test
+    public void restoringMissingLetterToArray() {
+        final List<String> result = List.of(
+                " 1.a",
+                " 2.b",
+                " 3.d",
+                " 4.e",
+                " 5.f",
+                " 6.g",
+                " 7.h",
+                " 8.j",
+                " 9.l",
+                "10.m",
+                "11.n",
+                "12.o",
+                "13.p",
+                "14.r",
+                "15.s",
+                "16.t",
+                "17.u",
+                "18.v",
+                "19.w",
+                "20.x",
+                "21.y",
+                "22.z");
+
+        final List<String> expected = new ArrayList();
+        final Mono<String> missing = Mono.just("s");
+
+        Flux.fromArray(words.toArray())
+                .flatMap(word -> Flux.fromArray(word.toString().split("")))
+                .concatWith(missing)
+                .distinct()
+                .sort()
+                .zipWith(Flux.range(1, Integer.MAX_VALUE), (string, count) -> String.format("%2d.%s", count, string))
+                .subscribe(expected::add);
+
+        result.stream()
+                .forEach(expected::remove);
+
+        Assert.assertTrue(expected.isEmpty());
+    }
+
+    @Test
+    public void subscribeNonBlocking() {
+        Flux<String> helloPauseWorld =
+                Mono.just("Hello")
+                    .concatWith(Mono.just("wo"))
+                    .concatWith(Mono.just("rl"))
+                    .concatWith(Mono.just("d!"))
+                    .delayElements(Duration.ofMillis(500));
+
+        helloPauseWorld.subscribe(it ->{
+            Assert.assertTrue(false);
+        });
+    }
+
+    @Test
+    public void subscribeBlockingWithToStream() {
+        String expected = "Helloworld!";
+        StringBuilder result = new StringBuilder();
+
+        Mono.just("Hello")
+                .concatWith(Mono.just("wo"))
+                .concatWith(Mono.just("rl"))
+                .concatWith(
+                        Mono.just("d!")
+                            .delayElement(Duration.ofMillis(500))
+                )
+                .toStream()
+                .forEach(it -> result.append(it));
+
+        Assert.assertEquals(expected,result.toString());
+    }
 }
