a381568edf1e9a27804456e9f3aadd028360a5ef
vicboma1 <victorbolinchesmarin@gmail.com>
2017-09-26 10:02:05 +0200

    ConectableFluxTest connect tests update readme

--- src/test/java/demo1/ConnectableFluxTest.java
+++ src/test/java/demo1/ConnectableFluxTest.java
@@ -24,17 +24,27 @@ public class ConnectableFluxTest {
 
     @Test
     public void connect() throws Exception {
+        final String done_subscribing = "done subscribing";
+        final String will_now_connect = "will now connect";
+        final String subscribed_to_source_ = "subscribed to source ";
 
-        final StringBuilder result = new StringBuilder().append(1).append(1).append(1).append(2).append(2).append(2).append(3).append(3).append(3);
+        final StringBuilder expected = new StringBuilder()
+                .append(done_subscribing)
+                .append(will_now_connect)
+                .append(subscribed_to_source_)
+                .append(1).append(1).append(1)
+                .append(2).append(2).append(2)
+                .append(3).append(3).append(3);
 
-        final StringBuilder expected = new StringBuilder();
+        final StringBuilder result = new StringBuilder();
 
         final Flux<Integer> source =
                 Flux.range(1, 3)
                 .doOnSubscribe(s -> {
-                    System.out.println("subscribed to source ");
+                    result.append(subscribed_to_source_);
+                    System.out.println(subscribed_to_source_);
                 })
-                .doOnNext(expected::append)
+                .doOnNext(result::append)
                 .doOnEach(it -> System.out.println("doOnEach: "+it)) //all description operations
                 .doOnError(System.err::println)
                 .doFinally(it -> System.out.println("doFinally: "+it));
@@ -43,22 +53,24 @@ public class ConnectableFluxTest {
         final ConnectableFlux<Integer> co = source.publish();
 
         co.subscribe(
-                expected::append, // out
+                result::append, // out
                 System.err::println,  // err
                 () -> {}  //completed callback
         );
 
         co.subscribe(
-                expected::append, // out
+                result::append, // out
                 System.err::println,  // err
                 () -> {}  //completed callback
         );
 
-        System.out.println("done subscribing");
+        result.append(done_subscribing);
+        System.out.println(done_subscribing);
 
         Thread.sleep(500);
 
-        System.out.println("will now connect");
+        result.append(will_now_connect);
+        System.out.println(will_now_connect);
 
         co.connect();
 
@@ -68,6 +80,101 @@ public class ConnectableFluxTest {
 
     @Test
     public void autoConnect() throws Exception {
+        final String subscribed_to_source_ = "subscribed to source ";
+        final String subscribed_first = "subscribed first";
+        final String subscribing_second = "subscribing second";
+
+        final StringBuilder expected = new StringBuilder()
+                .append(subscribed_first)
+                .append(subscribing_second)
+                .append(subscribed_to_source_)
+                .append(1).append(1).append(1)
+                .append(2).append(2).append(2)
+                .append(3).append(3).append(3);
+
+        final StringBuilder result = new StringBuilder();
+
+        final Flux<Integer> source =
+                Flux.range(1, 3)
+                        .doOnSubscribe(s -> {
+                            result.append(subscribed_to_source_);
+                        })
+                        .doOnNext(result::append)
+                        .doOnEach(it -> System.out.println("doOnEach: "+it)) //all description operations
+                        .doOnError(System.err::println)
+                        .doFinally(it -> System.out.println("doFinally: "+it));
+
+        final Flux<Integer> autoCo = source.publish().autoConnect(2);
+
+        autoCo.subscribe(
+                result::append, // out
+                System.err::println,  // err
+                () -> {}  //completed callback
+        );
+
+        result.append(subscribed_first);
+        System.out.println(subscribed_first);
+
+        Thread.sleep(500);
+
+        result.append(subscribing_second);
+        System.out.println(subscribing_second);
+
+        autoCo.subscribe(
+                result::append, // out
+                System.err::println,  // err
+                () -> {}  //completed callback
+        );
+
+        Assert.assertEquals(expected.toString(),result.toString());
+
+    }
+
+    @Test
+    public void autoConnectMinSubscribed() throws Exception {
+        final String subscribed_to_source_ = "subscribed to source ";
+        final String subscribed_first = "subscribed first";
+        final String subscribing_second = "subscribing second";
+
+        final StringBuilder expected = new StringBuilder()
+                .append(subscribed_first)
+                .append(subscribing_second);
+
+        final StringBuilder result = new StringBuilder();
+
+        final Flux<Integer> source =
+                Flux.range(1, 3)
+                        .doOnSubscribe(s -> {
+                            result.append(subscribed_to_source_);
+                        })
+                        .doOnNext(result::append)
+                        .doOnEach(it -> System.out.println("doOnEach: "+it)) //all description operations
+                        .doOnError(System.err::println)
+                        .doFinally(it -> System.out.println("doFinally: "+it));
+
+        final Flux<Integer> autoCo = source.publish().autoConnect(4);
+
+        autoCo.subscribe(
+                result::append, // out
+                System.err::println,  // err
+                () -> {}  //completed callback
+        );
+
+        result.append(subscribed_first);
+        System.out.println(subscribed_first);
+
+        Thread.sleep(500);
+
+        result.append(subscribing_second);
+        System.out.println(subscribing_second);
+
+        autoCo.subscribe(
+                result::append, // out
+                System.err::println,  // err
+                () -> {}  //completed callback
+        );
+
+        Assert.assertEquals(expected.toString(),result.toString());
 
     }
 }
